9870231520592877e10c309140956866828b3303
Gaurav <44445154+gaurav219@users.noreply.github.com>
2019-06-29 00:31:42 +0530

    Added files

---
+++ script.js
@@ -0,0 +1,19 @@
+
+const c = document.getElementById('c')
+const equal = document.getElementById('equal')
+const all = document.querySelector('.all')
+all.addEventListener('click', function (e) {
+    if (e.target.matches('button')) {
+        if (e.target.innerText != 'C' && e.target.innerText != '=') {
+            input.value += e.target.innerText
+        }
+        if (e.target.innerText == 'C') {
+            input.value = ""
+        }
+        else if (e.target.innerText == '=') {
+            input.value = eval(input.value)
+        }
+    }
+})
+
+
